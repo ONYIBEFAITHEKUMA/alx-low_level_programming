0cf95a6a3a247bdfd9b230c290342d7d4f25866a
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-16 11:30:03 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Return 0(sucsee)
- *
+ * Return: 0(succsee)
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
