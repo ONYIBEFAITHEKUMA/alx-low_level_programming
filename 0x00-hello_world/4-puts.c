b9102857a646a065bdb57187e688b95e836f3851
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-16 09:53:26 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/**
- * using a funtion put
- * to print
- * Programming is like building a multilingual puzzle
- */
-int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
-}
 
+/**
+ *
+ * main - entry point
+ *
+ * success Always return (0)
+ *
+ */
+int main(void) {
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
