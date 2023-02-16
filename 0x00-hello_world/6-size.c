71bfe176c3e86d710e2c84b7bc40c4e95687cff0
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-16 11:56:37 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {int a; long int b; long long int c; char d; float f;
+	printf("size of a char: %lu bytes(s)\n";
+	printf("size of an int: %lu bytes(s)\n";
+	printf("size of long int: %lu bytes(s)\n";
+	printf("size of long long int: %lu bytes(s)\n";
+	printf("size of a float: %lu bytes(s)\n";
+	return (0);
+	}
+
