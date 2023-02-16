8f1814d9fee699640b690e053c3f09a5cf4b6ea3
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-16 12:41:35 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * code that print  data without using printf or puts function 
- * Return 1
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-return 1;
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
