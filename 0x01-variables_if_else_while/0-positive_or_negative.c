309fdcf6da90d4c700e5b7181205502347a626f0
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-17 17:28:08 -0800

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/* headers goes there */
+
+/* betty style doc files goes there */
+/**
+ * main - Entery point
+ *
+ * Return: Always 0(suceess)
+ *
+ */
+int main(void)
+{
+         int n;
+	 srand(time(0));
+	 n = rand() - RAND_MAX / 2;
+	 /* your code goes in here*/
+	 if (n > 0);
+	 	printf("%\d is positive\n", n);
+	else if (n < 0);
+		printf("%\d is negative\n", n);
+	else printf("%\d is zero\n"' n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+An if statement can be followed by an optional else if...else statement, which is very useful to test various conditions using single if...else if statement.
