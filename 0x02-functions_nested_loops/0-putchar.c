24dcf59fb039eae9cc2c253913cb84ab7ae04e73
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-21 09:02:49 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>>
+#include <main.h>>
 /**
  *  main - Entry point
  *
@@ -7,7 +7,7 @@
 int main(void)
 {
 	/* C program to demonstrate putchar() method */
-	char fa = "faith";
+	char fa = "1";
 	/* character to stdout */
 	while (fa)
 	{
@@ -15,5 +15,6 @@ int main(void)
 		fa++;
 	}
 	_putchar('\n');
+
 		return (0);
 }
