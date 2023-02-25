397d9a826c7428d6f7493ab10ff5ab0d73cdf4fa
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-24 20:54:14 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - function that print isdigit
+ * @c: parameter to be checked
+ * Return: 1 on succes, 0 on fail
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
