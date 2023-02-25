b59dfc335004f352c6e9a127851c1d688defbab7
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-24 21:02:33 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies 2 numbers
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ * Return: the mutiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
