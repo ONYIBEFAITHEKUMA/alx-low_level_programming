0dd076011594d093abe6ff75bd5c4a58b6fcaedf
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-02-27 05:14:00 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - funtion to be checked
+ *
+ * @n: parameters to be checked 
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Every time you declare a variable, the computer will reserve memory for this variable. The memory reserved will then store the value of the variable.
