6b254d50810479f96e6cf4229f3aa561b93117f7
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-03-10 16:56:10 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: numbers of argument
+ *
+ * @argv: array of argument
+ *
+ * Return: alway 0 (success)
+ */
+int main(int argc __attribute__ ((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
