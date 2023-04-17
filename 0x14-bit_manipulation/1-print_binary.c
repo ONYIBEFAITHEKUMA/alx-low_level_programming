8a087b22c5d110616c4b965fb47114115ac1722c
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-04-17 05:15:02 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_binary - function thayt print binary numbers.
+ *
+ * @n: unsignened long int.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	(n &) ? _putchar('1') : _putchar('0');
+}
