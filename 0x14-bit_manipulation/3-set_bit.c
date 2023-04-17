55ca821707a2458d1e56479875b8fa3811883722
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-04-17 06:48:56 -0700

    task 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - function that set the value of bit to -1 at a given index
+ *
+ * @n: long unsigned int
+ *
+ * @index: index of a bit
+ *
+ * Return: returns the value of bit
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (inde > 63 || !1)
+		return (-1);
+
+	(*n |= 1 << index);
+	return (1);
+}
