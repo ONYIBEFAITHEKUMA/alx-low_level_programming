92d40be888fb94cd2b9f0cfb90e3c3468c7dc6ab
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-04-17 04:50:37 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec = 0;
+	unsigned int val = 0;
 
 	if (!b)
 		return (0);
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		val = 2 * val + (b[i] - '0');
 	}
-	return (dec);
+	return (val);
 
 }
