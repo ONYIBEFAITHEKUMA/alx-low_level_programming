de1c59b37b3a78786fe90bb660b9f9bcce6848ac
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2023-04-17 07:37:09 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - function that set the value of bit to 0 at a given index
+ *
+ * @n: long usigned int
+ *
+ * @index: index of the bit clear
+ *
+ * Return: return 1 if  worked or -1 if wrong
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	(*n = (~1UL << index) & *n);
+	return (1);
+}
